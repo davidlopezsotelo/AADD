ca25b209cdbc40273e99356396c1e3a8006c68a9
David <davidaquarium@yahoo.es>
2021-06-24 00:49:17 +0200

    conexiones a base de datos 24/06/21

--- AccesoADatos/src/BasesDatos/PruebaConexionBBDD.java
+++ AccesoADatos/src/BasesDatos/PruebaConexionBBDD.java
@@ -1,11 +1,14 @@
 package BasesDatos;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+
+
 
 public class PruebaConexionBBDD {
 
 	public static void main(String[] args) {
+		
+		//----PRIMER PASO: CONECTARSE A LA BASE DE DATOS---------------------------------------------------
 		
 		String URL="jdbc:mysql://localhost:3306/m06?useSSL=false";
 		
@@ -18,9 +21,52 @@ public class PruebaConexionBBDD {
 			
 			System.out.println("Conectando con base de datos.");
 			
+			/*
+			 * Creamos un objeto de tipo Connection para conectarnos a la base de datos.
+			 * utilizando la clase DriverManager.
+			 */
 			Connection Conexion=DriverManager.getConnection(URL, User, Passs);
 			
 			System.out.println("Conexion establecida con exito");
+			
+		//-------SEGUNDO PASO: CREAR OBJETO STATEMENT---------------
+			
+			
+			/*
+			 * Un objeto statement es el que permite ejecutar sentecias SQL.
+			 */
+			
+			Statement miStatement = Conexion.createStatement();
+			
+		//-----TERCER PASO: EJECUTAR SENTENCIA SQL:
+			
+			/*
+			 * Usamos el metodo ExecuteQuery, que nos devolverá
+			 * un objeto de tipo resulset.
+			 */
+					
+			ResultSet miResulset=miStatement.executeQuery("SELECT*FROM alumnos");
+			
+		//----CUARTO PASO:RECORRER EL RESULSET.--------------------
+			
+			/*
+			 * utilizaremos un bucle while, para recorrerlo.
+			 */
+			
+			while(miResulset.next()) {//mientras halla un siguiente registro en la tabla...
+				
+				/*
+				 * le pedimos que nos imprima en consola los resultados
+				 * de las tablas que le indiquemos, que tiene almacenadas en
+				 * el resultset.
+				 */
+				
+                                             //nombre de la columna				
+				System.out.println(miResulset.getString("idAlumno")+ " "+ miResulset.getString("nombre")+ " "+ miResulset.getString("edad"));
+				
+				
+			}
+			
 			
 		}catch(Exception e){
 			
@@ -29,6 +75,8 @@ public class PruebaConexionBBDD {
 			e.printStackTrace();
 			
 		}
+		
+		
 	}
 
 }
